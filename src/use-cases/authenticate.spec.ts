494f5ee1973acefcea6ec5e5f476730ef2dbcb56
GabMaffei <gabriel.maffei.alm@edu.univali.br>
2025-07-20 16:07:30 -0300

    adiciona caso de uso de autenticação com tratamento de erro para credenciais inválidas e implementa testes

---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,55 @@
+import { expect, it, describe, beforeEach } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { AuthenticateUseCase } from './authenticate'
+import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials'
+import { UsersRepository } from '@/repositories/users-repository'
+
+let usersRepository: UsersRepository
+let sut: AuthenticateUseCase
+
+describe('Authenticate Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new AuthenticateUseCase(usersRepository)
+  })
+
+  it('should be able to authenticate', async () => {
+    await usersRepository.create({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password_hash: await hash('securepassword', 6),
+    })
+
+    const { user } = await sut.execute({
+      email: 'john.doe@example.com',
+      password: 'securepassword',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    await expect(
+      sut.execute({
+        email: 'john.doe@example.com',
+        password: 'securepassword',
+      }),
+    ).rejects.instanceOf(InvalidCredentialsError)
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    await usersRepository.create({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password_hash: await hash('securepassword', 6),
+    })
+
+    await expect(
+      sut.execute({
+        email: 'john.doe@example.com',
+        password: 'wrongpassword',
+      }),
+    ).rejects.instanceOf(InvalidCredentialsError)
+  })
+})
--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,15 +1,20 @@
-import { expect, it, describe } from 'vitest'
+import { expect, it, describe, beforeEach } from 'vitest'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists'
+import { UsersRepository } from '@/repositories/users-repository'
+
+let usersRepository: UsersRepository
+let sut: RegisterUseCase
 
 describe('Register Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new RegisterUseCase(usersRepository)
+  })
   it('should hash the user password upon registration', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
-
-    const { user } = await registerUseCase.execute({
+    const { user } = await sut.execute({
       name: 'John Doe',
       email: 'john.doe@example.com',
       password: 'securepassword',
@@ -19,10 +24,7 @@ describe('Register Use Case', () => {
   })
 
   it('should hash the user password upon registration', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
-
-    const { user } = await registerUseCase.execute({
+    const { user } = await sut.execute({
       name: 'John Doe',
       email: 'john.doe@example.com',
       password: 'securepassword',
@@ -36,19 +38,16 @@ describe('Register Use Case', () => {
   })
 
   it('should not be able to register with same email twice', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
-
     const email = 'john.doe@example.com'
 
-    await registerUseCase.execute({
+    await sut.execute({
       name: 'John Doe',
       email,
       password: 'securepassword',
     })
 
     await expect(
-      registerUseCase.execute({
+      sut.execute({
         name: 'John Doe',
         email,
         password: 'securepassword',
