0a261cd9a04b6187dcbde41f4e41aba4f4dc3ace
GabMaffei <gabriel.maffei.alm@edu.univali.br>
2025-07-20 13:57:36 -0300

    adiciona repositório de usuários em memória e implementa testes para o caso de uso de registro

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,25 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User, Prisma } from 'generated/prisma'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+
+  async create(data: Prisma.UserCreateInput) {
+    const user: User = {
+      id: 'user-id',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+
+    this.items.push(user)
+
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+    return user || null
+  }
+}
